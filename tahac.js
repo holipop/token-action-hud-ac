238214d8022e24e99d17466f27dbf3f0f7ece56c
Sunny <adamcroslow@gmail.com>
2023-12-06 04:51:39 -0600

    i guess its done?

--- tahac.js
+++ tahac.js
@@ -2,25 +2,18 @@
 // by Holipop
 
 
-/** Constants
- */
-const ID = 'token-action-hud-ac'
-const CORE_ID = 'token-action-hud-core'
-const REQUIRED_CORE_MODULE_VERSION = '1.5'
-
 /** Classes
  */
 let SystemManager, ActionHandler, RollHandler;
 
 Hooks.on('tokenActionHudCoreApiReady', async core => {
 
-    /**
+    /** 
      * Extends Token Action HUD Core's SystemManager class
      */
     SystemManager = class SystemManager extends core.api.SystemManager {
 
         /** Returns an instance of the ActionHandler to Token Action HUD Core
-         * Called by Token Action HUD Core
          * @override
          * @returns {class} The ActionHandler instance
          */
@@ -30,7 +23,6 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
 
         /** Returns a list of roll handlers to Token Action HUD Core
          * Used to populate the Roll Handler module setting choices
-         * Called by Token Action HUD Core
          * @override
          * @returns {object} The available roll handlers
          */
@@ -39,7 +31,6 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
         }
 
         /** Returns an instance of the RollHandler to Token Action HUD Core
-         * Called by Token Action HUD Core
          * @override
          * @param {string} id   The roll handler ID
          * @returns {class}     The RollHandler instance
@@ -49,19 +40,51 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
         }
 
         /** Returns the default layout and groups to Token Action HUD Core
-         * Called by Token Action HUD Core
+         * @override
          * @returns {object} The default layout and groups
          */
-        registerDefaults () {
-            return { }
+        async registerDefaults () {
+            const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+            const ids = ['kit']
+
+            const groups = ids.map(id => {
+                return { id, name: capitalize(id), type: 'system' }
+            })
+            const layout = [{
+                id: 'kit', 
+                nestId: 'kit',
+                name: capitalize('kit'),
+                groups: groups.map(group => {
+                    return { ...group, nestId: `kit_${group.id}` }
+                })
+            }]
+
+            console.log({ layout, groups })
+
+            return {
+                layout: [
+                    {
+                        id: 'kit', 
+                        nestId: 'kit',
+                        name: 'Kit',
+                        groups: [
+                            { id: 'kit', nestId: 'kit_kit', name: 'Kit', type: 'system' }
+                        ]
+                    }
+                ],
+                group: [
+                    { id: 'kit', name: 'Kit', type: 'system' }
+                ]
+            }
+            
+            return { layout, groups }
         }
 
         /** Register Token Action HUD system module settings
-         * Called by Token Action HUD Core
          * @override
          * @param {function} coreUpdate The Token Action HUD Core update function
          */
-        registerSettings (coreUpdate) { }
+        registerSettings (onChangeFunction) { }
 
     }
 
@@ -71,17 +94,33 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
     ActionHandler = class ActionHandler extends core.api.ActionHandler {
 
         /** Build system actions
-         * Called by Token Action HUD Core
          * @override
          * @param {array} ids
-         */a
-        async buildSystemActions (ids) {
+         */
+        async buildSystemActions (_) {
             if (!this.actor) return;
 
-            const categories = [...this.actor.system.categories]
+            for (const category of this.actor.system.categories) {
+                this.addGroup(
+                    { id: category.name, name: category.name, type: 'system' },
+                    { id: 'kit', type: 'system' }
+                )
+            }
 
-            console.log(this)
-            console.log(categories)
+            for (const item of this.actor.items) {
+                const action = {
+                    id: item._id,
+                    name: item.name,
+                    encodedValue: `item|${item._id}`,
+                    img: item.img
+                }
+                const group = {
+                    id: item.system.category,
+                    type: 'system'
+                }
+
+                this.addActions([action], group)
+            }
         }
 
     }
@@ -91,14 +130,17 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
      */
     RollHandler = class RollHandler extends core.api.RollHandler {
 
-        /** Handle action click
-         * Called by Token Action HUD Core when an action is left or right-clicked
+        /** Handle when an action is left or right-clicked
          * @override
          * @param {object} event
          * @param {string} code 
          */
         handleActionClick (event, code) {
+            const [, id] = code.split(this.delimiter)
+            const item = this.actor.getEmbeddedDocument("Item", id)
 
+            const post = (event.which === 3) // if right click was used
+            item.roll({ post });
         }
 
     }
@@ -108,6 +150,10 @@ Hooks.on('tokenActionHudCoreApiReady', async core => {
 
 /** Initialization
  */
+const ID = 'token-action-hud-ac'
+const CORE_ID = 'token-action-hud-core'
+const REQUIRED_CORE_MODULE_VERSION = '1.5'
+
 Hooks.on('tokenActionHudCoreApiReady', async () => {
     // Return the SystemManager and requiredCoreModuleVersion to Token Action HUD Core
     const module = game.modules.get(ID)
